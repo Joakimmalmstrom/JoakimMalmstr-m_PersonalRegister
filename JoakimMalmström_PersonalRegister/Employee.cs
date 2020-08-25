13973d36dc837df1f0104e3d3846b5f7be8a7562
Joakim Malmström <35744406+Joakimmalmstrom@users.noreply.github.com>
2020-08-25 15:26:41 +0200

    Add project files.

---
+++ JoakimMalmström_PersonalRegister/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoakimMalmström_PersonalRegister
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Name = new List<string>();
+            Salary = new List<int>();
+        }
+
+        public void AddEmployee()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Salary: ");
+            int salary = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Name.Add(name);
+            Salary.Add(salary);
+        }
+
+        public void CompileEmployee()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < Name.Count; i++)
+            {
+                Console.WriteLine("Employee " + (1 + i));
+                Console.WriteLine("Name: " + Name[i]);
+                Console.WriteLine("Salary: " + Salary[i]);
+                Console.WriteLine("");
+            }
+        }
+
+        List<string> Name;
+        List<int> Salary;
+    }
+}
---
+++ JoakimMalmström_PersonalRegister/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoakimMalmström_PersonalRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.WriteLine("How many employees would u like to register?");
+            int numberEmployees = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for (int i = 0; i < numberEmployees; i++)
+            {
+                employee.AddEmployee();
+            }
+
+            employee.CompileEmployee();
+
+            Console.Read();
+        }
+    }
+}
